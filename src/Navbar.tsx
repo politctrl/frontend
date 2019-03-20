7845b6014d87ff9784a4b8491ebebd2771701625
rnickson <m@mickson.me>
2019-03-20 16:16:11 +0100

    refactor: restyled navbar "logo"

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const AppNameLink = styled(Link)`
+  color: #000000;
+  text-decoration: none;
+
+  :hover {
+    text-decoration-line: underline;
+  }
+`;
 
 class Navbar extends Component {
   render() {
     return (
       <nav>
-        <Link to="/">
+        <AppNameLink to="/">
           <h1>PolitCtrl</h1>
-        </Link>
+        </AppNameLink>
       </nav>
     );
   }
